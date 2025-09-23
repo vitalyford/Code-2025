81f4c84dd53598881b5ed6dd98e2dffb0b503344
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2025-09-23 10:07:07 -0400

    Add Recursion class with factorial, backward, and rabbits methods

---
+++ Recursion/Recursion.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+public class Recursion {
+    public static double factorial(double num) {
+        // return num == 1 ? 1 : num * factorial(num - 1);
+        if (num == 1) 
+            return 1;
+        return num * factorial(num - 1);
+    }
+
+    public static String backward(String input) {
+        // return input.equals("") ? "" : backward(input.substring(1)) + input.charAt(0);
+        if (input.equals("")) {
+            return "";
+        }
+        return backward(input.substring(1)) + input.charAt(0);
+    }
+
+    public static int rabbits(int n) {
+        if (n <= 2) return 1;
+        return rabbits(n - 1) + rabbits(n - 2);
+    }
+
+    public static int rabbitsFor(int n) {
+        if (n <= 2) return 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int count = 0;
+
+        for (int i = 3; i <= n; i++) {
+            count = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = count;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(backward("A Santa lived as a devil at NASA"));
+        System.out.println(rabbitsFor(120));
+    }
+}
