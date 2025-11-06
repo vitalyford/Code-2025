22d69e7890a4ddc53dd3f11e77dbb6cfd369b4eb
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2025-11-06 10:08:17 -0500

    Add HashMap example methods for practice

--- HashMaps/HashMapExample.java
+++ HashMaps/HashMapExample.java
@@ -1,10 +1,13 @@
 package HashMaps;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
 
-class Data {
+class Data implements Comparable<Data> {
     String name;
     int age;
     String classLevel;
@@ -33,11 +36,37 @@ class Data {
         Data d = (Data)o;
         return d.name.equals(name) && d.age == age && d.classLevel.equals(classLevel);
     }
+
+    public int compareTo(Data o) {
+        // First, compare by name
+        // If names are the same, compare by age
+        // If ages are the same, compare by classLevel
+        if (name.compareTo(o.name) == 0) {
+            if (age == o.age) {
+                return classLevel.compareTo(o.classLevel);
+            }
+            return age - o.age;
+        }
+        return name.compareTo(o.name);
+    }
 }
 
 public class HashMapExample {
     public static void main(String[] args) {
-        HashMap<Data, String> hm = new HashMap<>();
+        TreeMap<Data, String> hm = new TreeMap<>();
+
+        ArrayList<Data> students = new ArrayList<>();
+
+        students.add(new Data("Parker", 21, "junior"));
+        students.add(new Data("Devin", 22, "senior"));
+        students.add(new Data("Andrew", 20, "sophomore"));
+
+        // Print before sorting
+        System.out.println(students);
+        // Sort the items in the collection (ArrayList)
+        Collections.sort(students);
+        // Print after sorting
+        System.out.println(students);
 
         // Keys must be unique
         // if they are not, the previous data based on the key 
@@ -50,14 +79,14 @@ public class HashMapExample {
             System.out.println(hm.get(new Data("Andrew", 20, "sophomore")));
         }
 
-        // for(Map.Entry<String, Data> pair : hm.entrySet()) {
-        //     pair.getKey();
-        //     pair.getValue();
+        // for(Map.Entry<Data, String> pair : hm.entrySet()) {
+        //     System.out.println(pair.getKey());
+        //     System.out.println(pair.getValue());
         // }
-        
-        // // hm.keySet();
-        // for (String key : hm.keySet()) {
-        //     hm.get(key); // get the value
-        // }
+
+        // hm.keySet();
+        for (Data key : hm.keySet()) {
+            hm.get(key); // get the value
+        }
     }
 }
